34fd8db14d95dc0583f01475432b2960f2d8c8c8
Himanshu Gupta <himanshu.iitr1@gmail.com>
2017-09-17 18:01:26 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -54,6 +54,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {Users} = require('./models/users');
@@ -83,6 +85,23 @@ app.get('/todos', (req, res) => {
       todos: todos
     });
   }, (err) => {res.status(400).send(err)});
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todo: todo});
+  }, (err) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(3000, () => {
