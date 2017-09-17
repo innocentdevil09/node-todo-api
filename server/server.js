b37e94d814c9ecbc6693e52caa9b7ac002766e58
Himanshu Gupta <himanshu.iitr1@gmail.com>
2017-09-17 16:06:17 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -77,6 +77,14 @@ app.post('/todos', (req, res) => {
   // console.log(req.body);
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({
+      todos: todos
+    });
+  }, (err) => {res.status(400).send(err)});
+});
+
 app.listen(3000, () => {
   console.log('Server running at port 3000');
 });
